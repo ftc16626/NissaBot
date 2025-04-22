600f579e2a512fa90d82f86ddba2ee5012ffbb3a
Ieatdogs993 <perryhembree@gmail.com>
2025-04-22 17:28:58 -0600

    Update Huskyzones.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Huskyzones.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Huskyzones.java
@@ -13,105 +13,26 @@ import org.firstinspires.ftc.robotcore.internal.system.Deadline;
 
 import java.util.concurrent.TimeUnit;
 
-@Autonomous(name = "not very specific name no confusion", group = "Shame")
+@Autonomous(name = "Huskyzones", group = "Shame")
 public class Huskyzones extends LinearOpMode {
-    private DcMotor LFMotor   = null;
-    private DcMotor         RFMotor  = null;
-    private DcMotor         LBMotor   = null;
-    private DcMotor         RBMotor  = null;
-    private DcMotor         rotateArm = null;
-    private DcMotor         extendArm = null;
-    private DcMotor         extendArm2 = null;
-    CRServo Wheel1;
-    CRServo Wheel2;
-
-    private ElapsedTime runtime = new ElapsedTime(); // runs time
-
-    // Calculate the COUNTS_PER_INCH for your specific drive train.
-    // Go to your motor vendor website to determine your motor's COUNTS_PER_MOTOR_REV
-    // For external drive gearing, set DRIVE_GEAR_REDUCTION as needed.
-    // For example, use a value of 2.0 for a 12-tooth spur gear driving a 24-tooth spur gear.
-    // This is gearing DOWN for less speed and more torque.
-    // For gearing UP, use a gear ratio less than 1.0. Note this will affect the direction of wheel rotation.
-    static final double     COUNTS_PER_DRIVE_MOTOR_REV   = 384.5;
-    static final double     COUNTS_PER_EXT_MOTOR_REV    = 537.7;
-    static final double     COUNTS_PER_ROT_MOTOR_REV    = 1993.6; // Find motor RPMS
-    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // No External Gearing.
-    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;
-    static final double     PULLEY_DIAMETER_INCHES = 1.5 ;// For figuring circumference
-    static final double     COUNTS_PER_INCH         = (COUNTS_PER_DRIVE_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double     COUNTS_PER_EXTINCH      = (COUNTS_PER_EXT_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            (PULLEY_DIAMETER_INCHES * 3.1415);
-    static final double     ROTATE_GEAR_REDUC = 2.0 ;
-    static final double     COUNTS_PER_DEGREE       = (COUNTS_PER_ROT_MOTOR_REV * ROTATE_GEAR_REDUC) / 360;
-    static final double     DRIVE_SPEED             = 1;
-    static final double     TURN_SPEED              = 0.5;
-    static final double     ROT_SPEED               = 1; // Variables for speeds
-
 
     public HuskyLens Husky;
 
     @Override
     public void runOpMode(){
-        RBMotor = hardwareMap.get(DcMotor.class, "RBMotor"); // You know this
-        RFMotor = hardwareMap.get(DcMotor.class, "RFMotor");
-        LBMotor = hardwareMap.get(DcMotor.class, "LBMotor");
-        LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
-        rotateArm = hardwareMap.get(DcMotor.class, "rotateArm");
-        extendArm = hardwareMap.get(DcMotor.class, "extendArm1");
-        extendArm2 = hardwareMap.get(DcMotor.class, "extendArm2");
-        Wheel1 = hardwareMap.get(CRServo.class, "Wheel1");
-        Wheel1.resetDeviceConfigurationForOpMode();
-        Wheel2 = hardwareMap.get(CRServo.class, "Wheel2");
-        Wheel2.resetDeviceConfigurationForOpMode();
 
+
+        // EncoderProgramExample object = new EncoderProgramExample()
         Husky = hardwareMap.get(HuskyLens.class, "huskylens");
         Husky.selectAlgorithm(HuskyLens.Algorithm.COLOR_RECOGNITION);
-        LFMotor.setDirection(DcMotor.Direction.REVERSE);
-        LBMotor.setDirection(DcMotor.Direction.REVERSE);
-        RFMotor.setDirection(DcMotor.Direction.FORWARD);
-        RBMotor.setDirection(DcMotor.Direction.FORWARD);
-        rotateArm.setDirection(DcMotor.Direction.REVERSE);
-        extendArm.setDirection(DcMotor.Direction.REVERSE);
-        extendArm2.setDirection(DcMotor.Direction.FORWARD);
 
-        LFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        LBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        RFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        RBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rotateArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        extendArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        extendArm2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // Resets encoders to zero at beginning of program
 
-        LFMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        LBMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        RFMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        RBMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rotateArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        extendArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        extendArm2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        telemetry.addData("Starting at",  "%7d :%7d",
-                LFMotor.getCurrentPosition(),
-                LBMotor.getCurrentPosition(),
-                RFMotor.getCurrentPosition(),
-                RBMotor.getCurrentPosition(),
-                rotateArm.getCurrentPosition(),
-                extendArm.getCurrentPosition(),
-                extendArm2.getCurrentPosition());
-        telemetry.update();
 
 
         waitForStart();
-        rotateArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        extendArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        RBMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        RFMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        LFMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        LBMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
     while (opModeIsActive()) {
-        EncoderProgramExample object=null;
+        EncoderProgramExample object = null;
         object.encoderDrive(0,0,0,0,0,0,false,0,0,0,0,0);
             int READ_PERIOD = 1;
             ;
@@ -145,6 +66,11 @@ public class Huskyzones extends LinearOpMode {
                 }
             }
             if (zone == 1) {
+                object.encoderDrive(0.7,0,15,15,15,15,true,0,0,0,0,5);
+                object.encoderDrive(0.7,0,21,21,21,21,false,0,0,0,0,5);
+                object.encoderDrive(0.7,0,0,0,0,0,false,10,13,1,-1,5);
+
+
 
 
             } //else if (zone == 3) {
